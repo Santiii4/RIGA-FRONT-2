bf4c67935c1ea3ef5002572fe881228e903af20a
Santi <sal.sanchez@alumno.etec.um.edu.ar>
2023-11-28 17:56:51 -0300

    Register funciona

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,95 +1,72 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState } from "react";
 import "./Register.css";
-import riga from "../../multimedia/riga.png";
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
 
 export default function Register() {
-  const [formData, setFormData] = useState({
-    email: '',
-    username: '',
-    password: '',
-    repeatPassword: '',
-  });
+  const [firstname, nombre]=useState("")
+  const [Password, contraseña] =useState("")
+  const [Email, correoElectronico]=useState("")
+  const [repeatPassword, repetirContraseña] =useState("")
+  const registro = async (e) =>{
+    e.preventDefault()
+    if (Password === repeatPassword){
+      try {
+        await axios.post("http://localhost:8080/api/users",{Email,Password,firstname})
+        alert("cuenta creada exitosamente")
+        
+      } catch (error) {
+        alert("no se pudo crear la cuenta")
+        
+      }
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      // Realizar la acción de registro utilizando axios u otra librería
-      const response = await axios.post('/api/register', formData);
-      console.log('Registro exitoso:', response.data);
-      // Aquí podrías redirigir al usuario a la página de inicio o hacer otras acciones necesarias
-    } catch (error) {
-      console.error('Error en el registro:', error);
-      // Manejar el error de registro, mostrar un mensaje, etc.
+      
     }
-  };
+    else{
+      alert("las contraseñas no son iguales")
+    }
+    
+  }
 
   return (
-    <body>
-      <div className="container1">
-        <div className="wrapper">
-          <div className="title"><span>Register</span></div>
-          <form onSubmit={handleSubmit}>
-            <div className="row">
-              <i className="fas fa-user"></i>
-              <input
-                type="email"
-                name="email"
-                placeholder="Email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="row">
-              <i className="fas fa-user"></i>
-              <input
-                type="text"
-                name="username"
-                placeholder="Username"
-                value={formData.username}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="row">
-              <i className="fas fa-lock"></i>
-              <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                value={formData.password}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="row">
-              <i className="fas fa-lock"></i>
-              <input
-                type="password"
-                name="repeatPassword"
-                placeholder="Repeat Password"
-                value={formData.repeatPassword}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="row button">
-              <input type="submit" value="Register" />
-            </div>
-            <div className="signup-link">Already a member? <Link to='/Login'>Signin now</Link></div>
-          </form>
+    <div className="login-container">
+      <form className="login-form" action="#" method="post">
+        <h2>Cree su cuenta</h2>
+        <div className="input-container">
+          <label htmlFor="nombre">Nombre</label>
+          <input type="text" id="nombre" name="nombre" value ={firstname} onChange={(e) => nombre (e.target.value)} required />
         </div>
-      </div>
-    </body>
+        <div className="input-container">
+          <label htmlFor="email">Correo Electrónico</label>
+          <input type="email" id="email" name="email" value ={Email} onChange={(e) => correoElectronico (e.target.value)} required />
+        </div>
+        <div className="input-container">
+          <label htmlFor="contrasena">Contraseña</label>
+          <input type="password" id="contrasena" name="contrasena" value ={Password} onChange={(e) => contraseña (e.target.value)} required />
+        </div>
+        <div className="input-container">
+          <label htmlFor="repetir-contrasena">Repetir Contraseña</label>
+          <input
+            type="password"
+            id="repetir-contrasena"
+            name="repetir-contrasena" value ={repeatPassword} onChange={(e) => repetirContraseña (e.target.value)}
+            required
+          />
+        </div>
+
+        <div className ="botoneslogin">
+          
+          <Link>
+            <button type="submit" onClick = {registro}>Crear cuenta </button>
+          
+          </Link>
+
+
+        </div>
+        
+        
+      </form>
+    </div>
   );
 }
