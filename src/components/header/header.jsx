90f1df327f8bdf51ac95ac84658890c6a29ccae5
Santiii4 <sal.sanchez@alumno.etec.um.edu.ar>
2024-03-11 10:35:47 -0300

    11-03-2024

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,34 +1,74 @@
-import "./header.css"
-import riga from "../../multimedia/riga.png";
-import {Link} from 'react-router-dom'
-import Carrito from "../../multimedia/carro-de-la-compra.png";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, IconButton, InputBase, Badge } from '@mui/material';
+import { ShoppingCart, Search } from '@mui/icons-material';
+import { Autocomplete } from '@mui/material';
+import logoImage from "../../multimedia/riga.png";
+import carritoImage from "../../multimedia/carro-de-la-compra.png";
+import "./header.css"; // Importa los estilos CSS aquí
 
-export default function Header () {
-    return(
+const productOptions = [
+    { title: 'Product 1' },
+    { title: 'Product 2' },
+    { title: 'Product 3' },
+];
+
+export default function Header() {
+    return (
         <div>
-            <body class="body"> 
-                <section class="sectionMain">
-                    <div class="divLogoTitulo">     
-                        <Link to= '/'><img class="logo" src={riga}alt="R.I.G.A" /></Link>
-                    </div>
+            <AppBar position="static">
+                <Toolbar>
+                    <Link to='/' style={{ textDecoration: 'none'}}>
+                        <img className="logo" src={logoImage} alt="R.I.G.A" />
+                    </Link>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}></Typography>
+                    <Autocomplete
+                        id="search-combo"
+                        options={productOptions}
+                        getOptionLabel={(option) => option.title}
+                        style={{ width: 300, marginRight: '2rem' }}
+                        renderInput={(params) => (
+                            <InputBase
+                                {...params}
+                                placeholder="Search for products..."
+                                sx={{
+                                    background: '#1b5e20',
+                                    fontFamily: 'Roboto Slab, serif',
+                                    padding: '0.5rem',
+                                    borderRadius: '4px',
+                                    '& input': {
+                                        color: '#FAF1E4',
+                                    },
+                                }}
+                            />
+                        )}
+                    />
                     <div>
-                        <ul class="BarraNav">
-                            <li class="liNav">
-                                <Link to= '/Products' class="linkInfo">Products</Link>
+                        <ul className="barraNav">
+                            <li className="liNav">
+                                <Link to='/Products' className="linkInfo">Products</Link>
                             </li>
-                            <li class="liNav">
-                                <Link to = '/Register' class="linkInfo">Register</Link>
+                            <li className="liNav">
+                                <Link to='/Register' className="linkInfo">Register</Link>
                             </li>
-                            <li class="liNav">
-                                <Link to = '/Login' class="linkInfo">Login</Link>
+                            <li className="liNav">
+                                <Link to='/Login' className="linkInfo">Login</Link>
                             </li>
-                            <div class="divLogoTitulo">     
-                                <Link to= '/Carrito'><img class="Carrito" src={Carrito}alt="Carrito" /></Link>
-                            </div>
+                            <li className="liNav">
+                                <Link to='/Carrito'>
+                                    <IconButton aria-label="show cart items" color="inherit">
+                                        <Badge badgeContent={4} color="error">
+                                            <ShoppingCart />
+                                        </Badge>
+                                    </IconButton>
+                                </Link>
+                            </li>
                         </ul>
                     </div>
-                </section>
-            </body>
+                </Toolbar>
+            </AppBar>
         </div>
     )
 }
+
+
